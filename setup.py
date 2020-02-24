428b2dc7b9ab7c3fa09b7a59dfc0235f1e205599
Ilya Koziev <inkoziev@gmail.com>
2020-02-24 08:52:33 +0300

    Обновление модели частеречной разметки с расширенным датасетом

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import io
 import setuptools
 
-with io.open("README.md", mode="r", encoding='utf-8') as fh:
-    long_description = fh.read()
+#with io.open("README.md", mode="r", encoding='utf-8') as fh:
+#    long_description = fh.read()
 
 setuptools.setup(
     name="rupostagger",
-    version="0.0.38",
+    version="0.0.40",
     author="Ilya Koziev",
     author_email="inkoziev@gmail.com",
     description="Part-of-Speech Tagger for Russian language",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
     url="https://github.com/Koziev/rupostagger",
     packages=setuptools.find_packages(),
     package_data={'rupostagger': ['rupostagger.config', 'rupostagger.model']},
