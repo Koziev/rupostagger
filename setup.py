5d55e8807a7b73b742820b6438230306a2aea208
Ilya Koziev <inkoziev@gmail.com>
2020-03-26 09:53:02 +0300

    модель переобучена на расширенном до 900 тыс сэмплов датасете

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="rupostagger",
-    version="0.0.42",
+    version="0.0.43",
     author="Ilya Koziev",
     author_email="inkoziev@gmail.com",
     description="Part-of-Speech Tagger for Russian language",
