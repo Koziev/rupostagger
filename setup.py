05c3e3a918e51602678d929e3633eb6b0a3f60eb
Ilya Koziev <inkoziev@gmail.com>
2020-06-28 13:57:13 +0300

    переобучение тэггера после чистки обучающего корпуса

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="rupostagger",
-    version="0.0.75",
+    version="0.0.76",
     author="Ilya Koziev",
     author_email="inkoziev@gmail.com",
     description="Part-of-Speech Tagger for Russian language",
